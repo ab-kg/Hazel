d8884dea0d347ec0aca12cd84607327051c2042f
ab-kg <abhishekskalagurki.231ee203@nitk.edu.in>
2025-03-18 22:37:08 +0530

    Setup Basic Application and Entry Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+// For use by Hazel Application
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+
+// Entry point 
+#include "Hazel/EntryPoint.h"
+// ---------------------------------
+
+
+
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+
+
+	Application* CreateApplication();
+
+}
+
+
+
+// we are making application virtual becuase this class will be parent class for the application class in sandbox .
